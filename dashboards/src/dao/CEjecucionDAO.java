665a31fc8293e570128cdceeb4ae5e025653aaf2
rafa <rafa@192.168.0.18>
2017-01-15 20:59:46 -0600

    Correcion

--- dashboards/src/dao/CEjecucionDAO.java
+++ dashboards/src/dao/CEjecucionDAO.java
@@ -467,7 +467,7 @@ public class CEjecucionDAO {
 				pstm1.setInt(8, mes);
 				pstm1.setInt(9, ejercicio);
 				pstm1.setInt(10, mes);
-				pstm1.setInt(10, entidad);
+				pstm1.setInt(11, entidad);
 				pstm1.setInt(12, ejercicio);
 				pstm1.setInt(13, entidad);
 				ResultSet results = pstm1.executeQuery();	
